855622753db3f67584c0c3ed079d0bb10bd03aa9
Arthur Fiorette <me@arthur.place>
2024-08-12 15:59:12 -0300

    docs: logo

--- example.js
+++ example.js
@@ -1,14 +1,14 @@
-//
-// Proposal: Safe Assignments
-//
-// Be able to handle errors in assignments without throwing exceptions, in a
-// easier way and without let variables + nesting try/catch blocks.
-//
+#
+#  Proposal: Safe Assignment Operator
+#
+#  Be able to handle errors in assignments without throwing exceptions, in a
+#  easier way and without let variables + nesting try/catch blocks.
+#
 
 const [error, data] ?= await fs.readFile('file.json')
-//                     await fs.readFile[Symbol.result]('file.json')
+#                      await fs.readFile[Symbol.result]('file.json')
 const [error, data] ?= fs.readFileSync('file.json')
-//                     fs.readFileSync[Symbol.result]('file.json')
+#                      fs.readFileSync[Symbol.result]('file.json')
 
 if (error) {
   handleError(error)
